75397b43ad59c180c9a5c013995c684919f44be6
Mario Ortega Romero <marioortro@gmail.com>
2022-01-28 19:54:32 +0100

    actualizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {GameListComponent} from "./components/game-list/game-list.component";
+import {GameFormComponent} from "./components/game-form/game-form.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/games/list'
+  },
+  {
+    path: 'games/list',
+    component: GameListComponent
+  },
+  {
+    path: 'games/add',
+    component: GameFormComponent
+  },
+  {
+    path: 'games/edit/:id',
+    component: GameFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { GameListComponent } from './components/game-list/game-list.component';
+import { GameFormComponent } from './components/game-form/game-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    GameListComponent,
+    GameFormComponent
   ],
   imports: [
     BrowserModule,
